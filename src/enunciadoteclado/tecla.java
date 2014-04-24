39cb4c2180fc45dade179b1fa73e6be3a4e0c71e
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2014-04-24 14:06:49 +0200

    git add distancia.java posicion.java tecla.java teclado.java

---
+++ src/enunciadoteclado/tecla.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package enunciadoteclado;
+
+/**
+ *
+ * @author gabriel
+ */
+public class tecla {
+    
+    private char nombreTecla;
+    private boolean tiene4lados;
+    
+    
+}
---
+++ src/enunciadoteclado/teclado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package enunciadoteclado;
+
+/**
+ *
+ * @author gabriel
+ */
+
+enum TipoTeclado {
+    rectangular,
+    hexagonal,
+    dosHexagonos,
+}
+
+public class teclado {
+    
+    private TipoTeclado formaTeclado;
+    private int numFilas;
+    private int numCols;
+    
+    
+}
