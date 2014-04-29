55bdcd2306544e5475caa15fa2ddaac9633cc726
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2014-04-29 20:25:45 +0200

    [GABI] modified tecla.java

--- src/enunciadoteclado/tecla.java
+++ src/enunciadoteclado/tecla.java
@@ -20,20 +20,38 @@ public class tecla {
 //  Metodos
     public tecla() {}			    //constructor por defecto
     
+    /**
+     * Creadora tecla
+     * @param n:    nombre tecla
+     * @param x:    numero de fila
+     * @param y:    numero de columna
+     */
     public tecla(char n, int x, int y) {    //constructor
 	setNombre(n);
 	setFila(x);
 	setColumna(y);
     }
         
+    /**
+     * setNombre.   Asigna un nombre a la tecla
+     * @param n:    caracter/nombre de la tecla
+     */
     public void setNombre(char n) {
 	nombre = n;
     }
     
+    /**
+     * getNombre.   Obtiene el nombre de la tecla
+     * @return:     Devuelve el nombre de la tecla
+     */
     public char getNombre() {
 	return nombre;
     }
     
+    /**
+     * setFila.     Asigna la fila a la tecla
+     * @param x:    Numero de la fila de la tecla
+     */
     public void setFila(int x) {
 	if (x < 0) {
 	    System.out.println("Error: fila negativa");
@@ -42,10 +60,18 @@ public class tecla {
 	fila = x;
     }
     
+    /**
+     * getFila.     Obtiene la fila de la tecla
+     * @return:     Devuelve la fila de la tecla
+     */
     public int getFila() {
 	return fila;
     }
     
+    /**
+     * setColumna.  Asigna una columna a la tecla
+     * @param y:    Numero de la columna de la tecla
+     */
     public void setColumna(int y) {
 	if (y < 0) {
 	    System.out.println("Error: columna negativa");
@@ -54,6 +80,10 @@ public class tecla {
 	columna = y;
     }
 
+    /**
+     * getColumna.  Obtiene la fila de la tecla
+     * @return:     Devuelve la fila de la tecla
+     */
     public int getColumna() {
 	return columna;
     }
